1f7bf17db91cc9e40b35048b5b89f52248b32d33
Marouane Dbibih <marouane.dbibih@outlook.com>
2025-09-15 19:35:47 +0100

    Remove the React frontend

--- frontend/global.d.ts
+++
@@ -1,7 +0,0 @@
-// global.d.ts
-interface Window {
-    env: {
-      REACT_APP_API_ENDPOINT: string;
-      REACT_APP_GRAPHQL_URI: string;
-    };
-  }
--- frontend/src/config/apollo.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { env } from "./env";
-
-const qraphqlClient = new ApolloClient({
-  uri: env.REACT_APP_GRAPHQL_URI,
-  cache: new InMemoryCache(),
-});
-
-export default qraphqlClient;
